c30c6230d2061121f8b83dd2f3e013b0298cd59f
Camille Louédoc-Eyries <camille.eyries@gmail.com>
2022-08-20 00:55:45 +0200

    update xvfb script

--- template/vnc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-Xvfb :99 -screen 0 1024x768x16 -nolisten tcp -nolisten unix &
---
+++ template/xvfb.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export DISPLAY=:99
+Xvfb $DISPLAY -screen 0 1024x768x16 -nolisten tcp -nolisten unix &
