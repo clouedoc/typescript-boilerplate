b8c351fdcd28988ad2055903473a36e42e60f3c2
Camille Louédoc-Eyries <camille.eyries@gmail.com>
2022-08-22 22:43:49 +0200

    fix: fix bad code

--- template/src/packlets/database/env.ts
+++ template/src/packlets/database/env.ts
@@ -1,12 +1,14 @@
 import { z } from 'zod';
 
-export const dbEnv = z.object({
-  POSTGRES_USER: z.string().default('postgres'),
-  POSTGRES_PASSWORD: z.string(),
-  POSTGRES_HOSTNAME: z.string(),
-  POSTGRES_DATABASE: z.string().default('postgres'),
-  POSTGRES_PORT: z
-    .string()
-    .transform((v) => parseInt(v, 10))
-    .default(5432),
-});
+export const dbEnv = z
+  .object({
+    POSTGRES_USER: z.string().default('postgres'),
+    POSTGRES_PASSWORD: z.string(),
+    POSTGRES_HOSTNAME: z.string(),
+    POSTGRES_DATABASE: z.string().default('postgres'),
+    POSTGRES_PORT: z
+      .string()
+      .transform((v) => parseInt(v, 10))
+      .default('5432'),
+  })
+  .parse(process.env);
