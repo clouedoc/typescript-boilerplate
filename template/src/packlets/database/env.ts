08c996fbc0051049bf15fedb22615acfad8af9e2
Camille Louédoc-Eyries <clouedoc@tutanota.com>
2022-08-31 00:35:17 +0200

    fix(env): make db env optional

--- template/src/packlets/database/env.ts
+++ template/src/packlets/database/env.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const dbEnv = z
   .object({
     POSTGRES_USER: z.string().default('postgres'),
-    POSTGRES_PASSWORD: z.string(),
-    POSTGRES_HOSTNAME: z.string(),
+    POSTGRES_PASSWORD: z.string().optional(),
+    POSTGRES_HOSTNAME: z.string().default('localhost'),
     POSTGRES_DATABASE: z.string().default('postgres'),
     POSTGRES_PORT: z
       .string()
